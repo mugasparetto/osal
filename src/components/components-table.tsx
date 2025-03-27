88c05c3ffe8d2d43531bfd770eda04385ee637f9
mugasparetto <mu.gasparetto@hotmail.com>
2025-03-27 19:15:21 +0000

    moved fetchComponents to services file

--- src/components/components-table.tsx
+++ src/components/components-table.tsx
@@ -1,23 +1,5 @@
 import ComponentsTableClient from '@/components/components-table-client';
-
-async function fetchComponents(id: string) {
-  try {
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const response = await fetch(
-      `http://localhost:4000/weapons/${id}?_embed=components`
-    );
-
-    if (!response.ok) {
-      console.log(`Failed to fetch weapon: ${response.status}`);
-    }
-
-    return response.json();
-  } catch (error) {
-    console.log(error);
-    return {};
-  }
-}
+import { fetchComponents } from '@/lib/services';
 
 export default async function ComponentsTable({
   weaponId,
--- src/lib/services.ts
+++ src/lib/services.ts
@@ -1,6 +1,6 @@
 export async function fetchWeapons() {
   try {
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const response = await fetch('http://localhost:4000/weapons', {
       cache: 'force-cache',
@@ -14,5 +14,24 @@ export async function fetchWeapons() {
   } catch (error) {
     console.log(error);
     return [];
+  }
+}
+
+export async function fetchComponents(weaponId: string) {
+  try {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+
+    const response = await fetch(
+      `http://localhost:4000/weapons/${weaponId}?_embed=components`
+    );
+
+    if (!response.ok) {
+      console.log(`Failed to fetch weapon: ${response.status}`);
+    }
+
+    return response.json();
+  } catch (error) {
+    console.log(error);
+    return {};
   }
 }
