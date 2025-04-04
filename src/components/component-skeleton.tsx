8d6acd18c205567a4323f8c6e4af653ef855d9e9
mugasparetto <mu.gasparetto@hotmail.com>
2025-04-04 20:16:48 +0100

    suspense structure

---
+++ src/components/component-skeleton.tsx
@@ -0,0 +1,11 @@
+export default function ComponentSkeleton() {
+  return (
+    <>
+      <p>Loading</p>
+      <p>Loading</p>
+      <p>Loading</p>
+      <p>Loading</p>
+      <p>Loading</p>
+    </>
+  );
+}
